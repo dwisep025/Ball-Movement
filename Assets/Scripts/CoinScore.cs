4ca0f849c71ca26907be3ac3501d9cf77581928c
dwisep025 <dwiseptiawan46@gmail.com>
2021-10-10 20:27:30 +0700

    Scene9

---
+++ Assets/Scripts/CoinScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinScore : MonoBehaviour
+{
+    public static int scoreCoin;
+    Text scoreCoinText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreCoin = 0;
+        scoreCoinText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreCoinText.text = scoreCoin.ToString();
+    }
+}
