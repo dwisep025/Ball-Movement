93805eaee98cd92875e170a6e9419128306301a0
dwisep025 <dwiseptiawan46@gmail.com>
2021-10-10 19:57:20 +0700

    Scene7

---
+++ Assets/Scripts/BallControll.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class BallControll : MonoBehaviour
+{
+    public float moveSpeed = 0.1f;
+
+    public Rigidbody2D rb;
+
+    Vector3 mousePosition;
+    Vector2 position = new Vector2(0f, 0f);
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    }
+
+    private void FixedUpdate()  
+    {
+        rb.MovePosition(position);
+    }
+}
---
+++ Assets/Scripts/CoinControll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinControll : MonoBehaviour
+{
+
+    public GameObject Coin;
+    public float spawnRate = 0f;
+    public float nextSpawn = 0f;
+
+    private float[] linepos = { 3.57f, 2.23f, -0.83f, -3.29f, - 4.08f};
+    float randomX;
+    int randomY;
+
+    Vector2 whereToSpawn;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            randomX = Random.Range(-3f, 8f);
+            randomY = Random.Range(0, linepos.Length);
+            whereToSpawn = new Vector2(randomX, linepos[randomY]);
+            Instantiate(Coin, whereToSpawn, Quaternion.identity);
+
+            nextSpawn = Time.time + spawnRate;
+            Debug.Log(Time.time + spawnRate);
+        }
+    }
+}
